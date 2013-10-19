cf8b852ab2b422e09a59a1b4e34cbfc63735af4f
Charlie Sanders <sanderscharlie@gmail.com>
2013-10-18 23:27:24 -0500

    Capistrano deploy config for DynamicDB

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,54 +1,92 @@
-# NOTE: Some settings are defined in config/deploy/*.rb files.
-# Usage:
-#   cap deploy:setup ;# Create shared and releases directories in /var/www/sitename/.
-#   Edit your /var/www/YOURAPPNAME.com/shared/config/database.yml file with your database, username, password, and socket.
-#   cap deploy:cold ;# First time only.
-#   cap deploy:migrations ;# Any other time, if migrations need to be run during the deploy.
-#   cap deploy ;# Any other time, if no migrations are required.
-#   cap deploy:migrate ;# Run migrations manually.
-#   Set Apache's DocumentRoot /var/www/SITENAME/current/public
+require 'bundler/capistrano'
 
-task :production do
-  server prod_server, :app, :web, :db, primary: true
-  set :app_env, "production"
+# This capistrano deployment recipe is made to work with the optional
+# StackScript provided to all Rails Rumble teams in their Linode dashboard.
+#
+# After setting up your Linode with the provided StackScript, configuring
+# your Rails app to use your GitHub repository, and copying your deploy
+# key from your server's ~/.ssh/github-deploy-key.pub to your GitHub
+# repository's Admin / Deploy Keys section, you can configure your Rails
+# app to use this deployment recipe by doing the following:
+#
+# 1. Add `gem 'capistrano', '~> 2.15'` to your Gemfile.
+# 2. Run `bundle install --binstubs --path=vendor/bundles`.
+# 3. Run `bin/capify .` in your app's root directory.
+# 4. Replace your new config/deploy.rb with this file's contents.
+# 5. Configure the two parameters in the Configuration section below.
+# 6. Run `git commit -a -m "Configured capistrano deployments."`.
+# 7. Run `git push origin master`.
+# 8. Run `bin/cap deploy:setup`.
+# 9. Run `bin/cap deploy:migrations` or `bin/cap deploy`.
+#
+# Note: You may also need to add your local system's public key to
+# your GitHub repository's Admin / Deploy Keys area.
+#
+# Note: When deploying, you'll be asked to enter your server's root
+# password. To configure password-less deployments, see below.
+
+#############################################
+##                                         ##
+##              Configuration              ##
+##                                         ##
+#############################################
+
+GITHUB_REPOSITORY_NAME = 'r13-team-354'
+LINODE_SERVER_HOSTNAME = 'dynamic-db.r13.railsrumble.com'
+
+#############################################
+#############################################
+
+# General Options
+
+set :bundle_flags,               "--deployment"
+
+set :application,                "railsrumble"
+set :deploy_to,                  "/var/www/apps/railsrumble"
+set :normalize_asset_timestamps, false
+set :rails_env,                  "production"
+
+set :user,                       "root"
+set :runner,                     "www-data"
+set :admin_runner,               "www-data"
+
+# Password-less Deploys (Optional)
+#
+# 1. Locate your local public SSH key file. (Usually ~/.ssh/id_rsa.pub)
+# 2. Execute the following locally: (You'll need your Linode server's root password.)
+#
+#    cat ~/.ssh/id_rsa.pub | ssh root@LINODE_SERVER_HOSTNAME "cat >> ~/.ssh/authorized_keys"
+#
+# 3. Uncomment the below ssh_options[:keys] line in this file.
+#
+# ssh_options[:keys] = ["~/.ssh/id_rsa"]
+
+# SCM Options
+set :scm,        :git
+set :repository, "git@github.com:railsrumble/#{GITHUB_REPOSITORY_NAME}.git"
+set :branch,     "master"
+
+# Roles
+role :app, LINODE_SERVER_HOSTNAME
+role :db,  LINODE_SERVER_HOSTNAME, :primary => true
+
+# Add Configuration Files & Compile Assets
+after 'deploy:update_code' do
+  # Setup Configuration
+  run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+
+  # Compile Assets
+  run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
 end
 
-# We want to be able to deploy to staging and production environments.
-# NOTE: You'll need a config/deploy/*.rb file for each deploy environment.
-#set :stages, %w(staging production)
-#require 'capistrano/ext/multistage'
+# Restart Passenger
+deploy.task :restart, :roles => :app do
+  # Fix Permissions
+  sudo "chown -R www-data:www-data #{current_path}"
+  sudo "chown -R www-data:www-data #{latest_release}"
+  sudo "chown -R www-data:www-data #{shared_path}/bundle"
+  sudo "chown -R www-data:www-data #{shared_path}/log"
 
-# The name of the application, used in directory names.
-set :application, 'dynamicdb'
-
-# Define short names of servers we'll be deploying to.
-set :prod_server, 'dynamic-db.r13.railsrumble.com'
-set :user, 'dynamicdb'
-
-# Set SSH port, in case we use a non-standard port.
-set :port, 22
-
-set :scm, :git
-set :repository,  "git@github.com:railsrumble/r13-team-354.git"
-set :git_enable_submodules, 1         # Make sure git submodules are populated
-
-# Don't use sudo - run the startup scripts as myself. If we've set up the directory as sticky, with proper group ownership, this should work.
-set :use_sudo, false
-
-# Define the top-level directory into which to deploy.
-set :deploy_to, "/home/dynamicdb/dynamicdb/"
-
-namespace :deploy do
-  desc 'Restart Application (using Phusion Passenger)'
-  task :restart, :roles => :app do
-    run "touch #{current_path}/tmp/restart.txt"
-  end
-
-  desc "Package up asset files - compress JavaScript and CSS files into a single file for each"
-  task :package_assets, :roles => [:web] do
-    run "cd #{release_path} && rake asset:packager:build_all"
-  end
+  # Restart Application
+  run "touch #{current_path}/tmp/restart.txt"
 end
-after 'deploy:update_code', 'deploy:package_assets'
-after 'deploy', 'deploy:cleanup' # Leaves current deployment, plus the 4 previous.
-after 'deploy:migrations', 'deploy:cleanup'
