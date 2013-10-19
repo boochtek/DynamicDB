f2091fa9e2e65b9d859da6d03dac2765bc6c71a9
Craig Buchek <craig@boochtek.com>
2013-10-19 15:07:11 -0500

    Make sure a Database always starts with 1 table.

--- app/models/database.rb
+++ app/models/database.rb
@@ -1,6 +1,11 @@
 class Database < ActiveRecord::Base
   has_many :tables
 
+  def initialize()
+    super
+    tables << Table.new(database_id: id)
+  end
+
   def name
     read_attribute(:name) || 'New Database'
   end
