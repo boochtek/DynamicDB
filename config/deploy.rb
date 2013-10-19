68bc146ef2f3eff6b0e97b2c273baaa8a6a1384d
Charlie Sanders <sanderscharlie@gmail.com>
2013-10-18 23:53:07 -0500

    Unicorn fixups

--- config/deploy.rb
+++ config/deploy.rb
@@ -73,7 +73,6 @@ role :db,  LINODE_SERVER_HOSTNAME, :primary => true
 # Add Configuration Files & Compile Assets
 after 'deploy:update_code' do
   # Setup Configuration
-  run "cp #{shared_path}/config/database.yml #{release_path}/config/database.yml"
 
   # Compile Assets
   run "cd #{release_path}; RAILS_ENV=production bundle exec rake assets:precompile"
@@ -96,7 +95,6 @@ namespace :deploy do
     sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
     sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
-    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
   end
   after "deploy:setup", "deploy:setup_config"
