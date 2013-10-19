365361aad252add6eecddbe0647c5443d2ec87fd
Charlie Sanders <sanderscharlie@gmail.com>
2013-10-18 22:26:51 -0500

    YOLO capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,37 +8,35 @@
 #   cap deploy:migrate ;# Run migrations manually.
 #   Set Apache's DocumentRoot /var/www/SITENAME/current/public
 
-# If we don't specify the deploy environment, assume staging.
-set :default_stage, 'staging'
+task :production do
+  server prod_server, :app, :web, :db, primary: true
+  set :app_env, "production"
+end
 
 # We want to be able to deploy to staging and production environments.
 # NOTE: You'll need a config/deploy/*.rb file for each deploy environment.
-set :stages, %w(staging production)
-require 'capistrano/ext/multistage'
+#set :stages, %w(staging production)
+#require 'capistrano/ext/multistage'
 
 # The name of the application, used in directory names.
-set :application, 'NAME_OF_APPLICATION'
+set :application, 'dynamicdb'
 
 # Define short names of servers we'll be deploying to.
-set :prod_server, 'SERVER.TO.DEPLOY.TO'
+set :prod_server, 'dynamic-db.r13.railsrumble.com'
+set :user, 'dynamicdb'
 
 # Set SSH port, in case we use a non-standard port.
 set :port, 22
 
-# Define the Subversion repository.
-set :scm, :svn
-set :repository,  "svn+ssh://#{prod_server}/var/svn/#{application}/trunk"
-
-# Define a GIT repository.
-#set :scm, :git
-#set :repository,  "git@github.com:GITHUBUSERNAME/GITHUBREPOSITORYNAME.git"
-#set :git_enable_submodules, 1         # Make sure git submodules are populated
+set :scm, :git
+set :repository,  "git@github.com:railsrumble/r13-team-354.git"
+set :git_enable_submodules, 1         # Make sure git submodules are populated
 
 # Don't use sudo - run the startup scripts as myself. If we've set up the directory as sticky, with proper group ownership, this should work.
 set :use_sudo, false
 
 # Define the top-level directory into which to deploy.
-set :deploy_to, "/var/www/#{site_name}"
+set :deploy_to, "/home/dynamicdb/dynamicdb/"
 
 namespace :deploy do
   desc 'Restart Application (using Phusion Passenger)'
@@ -46,24 +44,11 @@ namespace :deploy do
     run "touch #{current_path}/tmp/restart.txt"
   end
 
-  desc 'Make symlink for database.yml'
-  task :symlink_database_yaml do
-    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml" 
-  end
-
-  desc "Create empty database.yml in shared path" 
-  task :create_database_yaml do
-    run "mkdir -p #{shared_path}/config" 
-    put '', "#{shared_path}/config/database.yml" 
-  end
-
   desc "Package up asset files - compress JavaScript and CSS files into a single file for each"
   task :package_assets, :roles => [:web] do
     run "cd #{release_path} && rake asset:packager:build_all"
   end
 end
-after 'deploy:setup', 'deploy:create_database_yaml'
-after 'deploy:update_code', 'deploy:symlink_database_yaml'
 after 'deploy:update_code', 'deploy:package_assets'
 after 'deploy', 'deploy:cleanup' # Leaves current deployment, plus the 4 previous.
 after 'deploy:migrations', 'deploy:cleanup'
