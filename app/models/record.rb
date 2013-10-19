14bd8e1f20e622aed5930ad2eedaf78adcd4ee65
Craig Buchek <craig@boochtek.com>
2013-10-19 18:23:31 -0500

    Add Record#update. Fix Record#data.

--- app/models/record.rb
+++ app/models/record.rb
@@ -6,6 +6,12 @@ class Record < ActiveRecord::Base
   # attribute :serialized_data, String
 
   def data
-    JSON.parse(read_attribute(:serialized_data)) || []
+    JSON.parse(read_attribute(:serialized_data)) rescue []
+  end
+
+  def update(options)
+    new_data = data.dup
+    new_data[Integer(options[:index])] = options[:value]
+    update_attribute(:serialized_data, JSON.unparse(new_data))
   end
 end
