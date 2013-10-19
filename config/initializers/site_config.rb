2539bb411776a5f635e5f9e3068ebc110eb9a8e2
Craig Buchek <craig@boochtek.com>
2013-10-18 21:23:37 -0500

    Basic site configuration.

--- config/initializers/site_config.rb
+++ config/initializers/site_config.rb
@@ -10,17 +10,17 @@ SITE_TITLE = SITE_NAME + ('production' == Rails.env ? '' : " (#{Rails.env.upcase
 
 # Hostname of the site. This will be used by Clearance to generate URLs in emails, to create a CSS signature, and possibly in several other places.
 # NOTE: It's highly recommended that you hard-code this.
-HOST = (ENV['SERVER_NAME']||'localhost').gsub(/[^A-Za-z0-9._-]/, '') # Ensure we get a valid hostname -- see http://shiflett.org/blog/2006/mar/server-name-versus-http-host.
+HOST = 'boochtek.com'
 
 # Create a CSS signature (http://archivist.incutio.com/viewlist/css-discuss/13291) for pages in your site.
 CSS_SIGNATURE = HOST.gsub(/[.]/, '-')
 
 # Years and name to list in copyright statements.
-COPYRIGHT_YEARS = '2008, 2009, 2010'
-COPYRIGHT_OWNER = Etc.getpwuid().gecos # NOTE: This probably isn't what you want -- you should really hard-code this.
+COPYRIGHT_YEARS = '2013'
+COPYRIGHT_OWNER = 'BoochTek, LLC'
 
 # Version of jQuery to use.
-JQUERY_VERSION = '2.0.2'
+JQUERY_VERSION = '2.0.3'
 
 # Google Analytics code. Set this to enable Google Analytics on the site.
 GOOGLE_ANALYTICS_CODE = nil
@@ -29,7 +29,7 @@ GOOGLE_ANALYTICS_CODE = nil
 GOOGLE_WEBMASTERS_CODE = nil
 
 # Email exceptions to this list of addresses, if ExceptionNotifier is loaded.
-EMAIL_EXCEPTIONS_TO = []
+EMAIL_EXCEPTIONS_TO = ['craig@boochtek.com']
 
 # Reply address for emails. Used by Clearance, and possibly other gems/plugins.
 DO_NOT_REPLY = "donotreply@#{HOST}"
