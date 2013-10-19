3bad7cded116574eba16b85d7fccb1481b9b7cc1
Charlie Sanders <sanderscharlie@gmail.com>
2013-10-19 00:54:05 -0500

    Button on homepage creates a new DB with empty name

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -69,6 +69,6 @@ class DatabasesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def database_params
-      params.require(:database).permit(:name)
+      params.fetch(:database, {})
     end
 end
