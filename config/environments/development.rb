8e8098d9177dd3df9d7b8b455215fb7e9c320cd5
Charlie Sanders <sanderscharlie@gmail.com>
2013-10-19 02:04:40 -0500

    Adds DataTables and jEditable javascript libraries

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,5 +26,6 @@ DynamicDB::Application.configure do
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
+  config.serve_static_assets = true
   config.assets.debug = true
 end
